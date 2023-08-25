5a1a64be21e15f10cb7a30cd933bdf0ad0dd5d70
Caleb Wightman <agentcboy@gmail.com>
2023-08-24 21:28:38 -0600

    cleaning up

--- src/App.js
+++ src/App.js
@@ -57,16 +57,15 @@ function Board( { xIsNext, squares, onPlay}) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = (currentMove % 2) === 0;
   const current = history[currentMove];
 
   function handlePlay(nextSquares) {
     const newHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(newHistory);
     setCurrentMove(newHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(move) {
